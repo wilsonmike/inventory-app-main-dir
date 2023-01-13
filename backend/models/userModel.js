cd50bbf709ad7882c183b2caa188bd5d322f216f
mike <michaelwilsonsj@hotmail.com>
2023-01-13 05:34:56 -0500

    setup jwt tokens & cookies, adding login route

---
+++ backend/models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email address"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: [6, "Password must be greater than 6 characters"],
+        // maxLength: [23, "Password must be less than 23 characters"]
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default: "https://i.ibb.co/4pDNDk1/avatar.png"
+    },
+    phone: {
+        type: String,
+        default: ""
+    },
+    bio: {
+        type: String,
+        maxLength: [250, "Password must be less than 250 characters"],
+        default: ""
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
